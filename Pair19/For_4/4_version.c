aa0dbf1eca700837e64fb90f35b5b91054fe0788
Pranita Raut <95748550+Pranita3434@users.noreply.github.com>
2023-12-28 14:39:10 +0530

    Add files via upload

---
+++ Pair19/For_4/4_version.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int counter;
+
+    for(counter=0;counter<=3;  )
+    {
+        printf("%d\n",counter);
+        counter++;
+    }
+
+    return 0;
+
+}
+
+/*
+Output : 0
+         1
+		 2 
+		 3
+*/
+
+
